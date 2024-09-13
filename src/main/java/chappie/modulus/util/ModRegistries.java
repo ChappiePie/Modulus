515d3ce0ec76d7f3c21524e1af200a959ac955c4
Chappie <53862142+ChappiePie@users.noreply.github.com>
2024-09-13 16:11:39 +0200

    port to fabric

---
+++ src/main/java/chappie/modulus/util/ModRegistries.java
@@ -0,0 +1,32 @@
+package chappie.modulus.util;
+
+import chappie.modulus.Modulus;
+import chappie.modulus.common.ability.base.AbilityType;
+import chappie.modulus.common.ability.base.Superpower;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.RangedAttribute;
+
+public class ModRegistries {
+
+    public static final Attribute FALL_RESISTANCE = registerAttribute("fall_resistance", new RangedAttribute("%s.fallResistance".formatted(Modulus.MODID), 0D, 0D, Double.MAX_VALUE));
+    public static final Attribute JUMP_BOOST = registerAttribute("jump_boost", new RangedAttribute("%s.jumpBoost".formatted(Modulus.MODID), 0D, 0D, Double.MAX_VALUE).setSyncable(true));
+    public static final SoundEvent YA = registerSoundEvent("ya");
+    public static final SoundEvent NET = registerSoundEvent("net");
+    public static final SoundEvent BUTTON = registerSoundEvent("button");
+
+    public static void init() {
+        AbilityType.init();
+        Superpower.init();
+    }
+
+    private static SoundEvent registerSoundEvent(String name) {
+        return Registry.register(BuiltInRegistries.SOUND_EVENT, Modulus.id(name), SoundEvent.createVariableRangeEvent(Modulus.id(name)));
+    }
+
+    private static Attribute registerAttribute(String name, Attribute attribute) {
+        return Registry.register(BuiltInRegistries.ATTRIBUTE, Modulus.id(name), attribute);
+    }
+}
