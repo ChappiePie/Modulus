539eabfd9cb459e737014cb7b137d23c54bdc920
Chappie <53862142+ChappiePie@users.noreply.github.com>
2026-06-28 18:25:42 +0200

    bump version

--- src/main/java/chappie/modulus/networking/client/ClientKeyInput.java
+++ src/main/java/chappie/modulus/networking/client/ClientKeyInput.java
@@ -5,29 +5,50 @@ import chappie.modulus.common.ability.base.Ability;
 import chappie.modulus.common.ability.base.condition.Condition;
 import chappie.modulus.common.capability.PowerCap;
 import chappie.modulus.util.KeyMap;
-import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
-public record ClientKeyInput(int entityId, String id, KeyMap keys) implements CustomPacketPayload {
+public class ClientKeyInput implements CustomPacketPayload {
 
     public static final ResourceLocation PACKET_ID = Modulus.id("client_key_input");
     public static final Type<ClientKeyInput> PACKET = new Type<>(PACKET_ID);
-    public static final StreamCodec<ByteBuf, ClientKeyInput> CODEC = StreamCodec.composite(
-            ByteBufCodecs.INT, ClientKeyInput::entityId,
-            ByteBufCodecs.STRING_UTF8, ClientKeyInput::id,
-            KeyMap.STREAM_CODEC, ClientKeyInput::keys,
-            ClientKeyInput::new
-    );
+    public static StreamCodec<FriendlyByteBuf, ClientKeyInput> CODEC = CustomPacketPayload.codec(ClientKeyInput::write, ClientKeyInput::new);
+    private final int entityId;
+    private final String id;
+    private final KeyMap keys;
+
+    public ClientKeyInput(int entityId, String id, KeyMap keys) {
+        this.entityId = entityId;
+        this.id = id;
+        this.keys = keys;
+    }
+
+    public ClientKeyInput(FriendlyByteBuf buf) {
+        this.entityId = buf.readInt();
+        this.id = buf.readUtf();
+        KeyMap map = new KeyMap();
+        for (KeyMap.KeyType type : KeyMap.KeyType.values()) {
+            map.setDown(type, buf.readBoolean());
+        }
+        this.keys = map;
+    }
 
     @Override
     public Type<? extends CustomPacketPayload> type() {
         return PACKET;
+    }
+
+    public void write(FriendlyByteBuf buf) {
+        buf.writeInt(this.entityId);
+        buf.writeUtf(this.id);
+        for (KeyMap.KeyType type : KeyMap.KeyType.values()) {
+            buf.writeBoolean(this.keys.isDown(type));
+        }
     }
 
     public void handle(LocalPlayer localPlayer) {
